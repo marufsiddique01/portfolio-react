85c62e98445a535ccbe15cdd086065fa1687f3c0
marufsiddique01 <maruf.siddique@northsouth.edu>
2022-06-01 05:56:06 +0600

    changed icon and navbar link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 
+import { Link } from 'react-scroll'
+
+
 import { FaBars, FaTimes, FaGithub, FaFacebook, FaLinkedin } from 'react-icons/fa'
 import { HiOutlineMail } from 'react-icons/hi'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
@@ -23,11 +26,25 @@ const Navbar = () => {
       {/* navbar menus */}
 
       <ul className='hidden md:flex text-white '>
-        <li> Home</li>
-        <li> About</li>
-        <li> Skills</li>
-        <li> Projects</li>
-        <li> Contact</li>
+        <li>
+          <Link to=" home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li> <Link to="skills" smooth={true} duration={500}>
+          Skills
+        </Link></li>
+        <li> <Link to="work" smooth={true} duration={500}>
+          Works
+        </Link></li>
+        <li> <Link to="contact" smooth={true} duration={500}>
+          Contact
+        </Link></li>
       </ul>
 
 
@@ -40,11 +57,31 @@ const Navbar = () => {
 
 
       <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center text-white'}>
-        <li className='py-6 text-4xl '> About</li>
-        <li className='py-6 text-4xl '> Home</li>
-        <li className='py-6 text-4xl '> Skills</li>
-        <li className='py-6 text-4xl '> Projects</li>
-        <li className='py-6 text-4xl '> Contact</li>
+        <li className='py-6 text-4xl '>
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className='py-6 text-4xl '>
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className='py-6 text-4xl '>
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className='py-6 text-4xl '>
+          <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+            Works
+          </Link>
+        </li>
+        <li className='py-6 text-4xl '>
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
       </ul>
 
 
