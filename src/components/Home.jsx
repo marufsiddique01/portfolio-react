b07e2d38114cd97aa6f6423264a2631435bc7fae
maruf <siddiquemaruf7@gmail.com>
2024-06-01 16:14:42 +0600

    formatters

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,38 +1,44 @@
-import React from 'react'
-import { HiArrowNarrowRight } from 'react-icons/hi'
-import { Link } from 'react-scroll'
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+import { Link } from "react-scroll";
 
 const Home = () => {
   return (
-    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
       {/* container */}
 
-      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <p className='text-[#ff5757] text-2xl'>Hi, my name is</p>
-        <h1 className='text-4xl sm:text-6xl font-bold text-[#ccd6f6]'>Maruf Siddique</h1>
-        <h2 className='text-2xl sm:text-5xl font-bold text-[#8892b0] pt-2'>I Design, Develop, and Analyze
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-[#ff5757] text-2xl">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-6xl font-bold text-[#ccd6f6]">
+          Maruf Siddique
+        </h1>
+        <h2 className="text-2xl sm:text-5xl font-bold text-[#8892b0] pt-2">
+          I Design, Develop, and Analyze
         </h2>
-        <p className='text-[#8892b0] py-4 max-w-[700px]'>
-
-          A Solution‑driven and experienced individual with multi‑dimensional expertise with experience in analyzing requirements and developing project structures and architectures to boost the
-          relationship and connections between the company and clients. Seeking opportunities to use my analytical, management, and development skills together to create cost‑effective solutions that
-          improve workflow efficiencies.
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          A Solution‑driven and experienced individual with multi‑dimensional
+          expertise with experience in analyzing requirements and developing
+          project structures and architectures to boost the relationship and
+          connections between the company and clients. Seeking opportunities to
+          use my analytical, management, and development skills together to
+          create cost‑effective solutions that improve workflow efficiencies.
         </p>
         <div>
-          <button href=''
-            className='text-white group border-2 px-6 py-3 my-2 flex flex-row items-center hover:bg-[#ff5757] hover:border-[#ff5757]'
+          <button
+            href=""
+            className="text-white group border-2 px-6 py-3 my-2 flex flex-row items-center hover:bg-[#ff5757] hover:border-[#ff5757]"
           >
             <Link to="work" smooth={true} duration={500}>
               View Works
             </Link>
-            <span className='group-hover:rotate-90 duration-300'>
-              <HiArrowNarrowRight className='ml-3' />
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3" />
             </span>
           </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,107 +1,132 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import { Link } from 'react-scroll'
+import { Link } from "react-scroll";
 
-import { FaBars, FaTimes, FaGithub, FaFacebook, FaLinkedin, FaInstagram } from 'react-icons/fa'
-import { HiOutlineMail } from 'react-icons/hi'
-import { BsFillPersonLinesFill } from 'react-icons/bs'
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
-import Logo from '../assets/logo.png'
+import Logo from "../assets/logo.png";
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false)
+  const [nav, setNav] = useState(false);
 
-  const handleClick = () => setNav(!nav)
+  const handleClick = () => setNav(!nav);
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]'>
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]">
       {/* logo of my self */}
       <div>
-        <img src={Logo} alt='Maruf Siddique' style={{ width: '120px', padding: '5px' }} />
+        <img
+          src={Logo}
+          alt="Maruf Siddique"
+          style={{ width: "120px", padding: "5px" }}
+        />
       </div>
       {/* navbar menus */}
 
-      <ul className='hidden md:flex text-white '>
+      <ul className="hidden md:flex text-white ">
         <li>
-          <Link to='home' smooth={true} duration={500}>
+          <Link to="home" smooth={true} duration={500}>
             Home
           </Link>
         </li>
         <li>
-          <Link to='about' smooth={true} duration={500}>
+          <Link to="about" smooth={true} duration={500}>
             About
           </Link>
         </li>
         <li>
-          {' '}
-          <Link to='skills' smooth={true} duration={500}>
+          {" "}
+          <Link to="skills" smooth={true} duration={500}>
             Skills
           </Link>
         </li>
         <li>
-          {' '}
-          <Link to='work' smooth={true} duration={500}>
+          {" "}
+          <Link to="work" smooth={true} duration={500}>
             Works
           </Link>
         </li>
         <li>
-          {' '}
-          <Link to='contact' smooth={true} duration={500}>
+          {" "}
+          <Link to="contact" smooth={true} duration={500}>
             Contact
           </Link>
         </li>
       </ul>
 
       {/* hamburger */}
-      <div onClick={handleClick} className='md:hidden z-10 text-white'>
+      <div onClick={handleClick} className="md:hidden z-10 text-white">
         {!nav ? <FaBars /> : <FaTimes />}
       </div>
 
       {/* mobile menu */}
 
-      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center text-white'}>
-        <li className='py-6 text-4xl '>
-          <Link onClick={handleClick} to='home' smooth={true} duration={500}>
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center text-white"
+        }
+      >
+        <li className="py-6 text-4xl ">
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
             Home
           </Link>
         </li>
-        <li className='py-6 text-4xl '>
-          <Link onClick={handleClick} to='about' smooth={true} duration={500}>
+        <li className="py-6 text-4xl ">
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
             About
           </Link>
         </li>
-        <li className='py-6 text-4xl '>
-          <Link onClick={handleClick} to='skills' smooth={true} duration={500}>
+        <li className="py-6 text-4xl ">
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
             Skills
           </Link>
         </li>
-        <li className='py-6 text-4xl '>
-          <Link onClick={handleClick} to='work' smooth={true} duration={500}>
+        <li className="py-6 text-4xl ">
+          <Link onClick={handleClick} to="work" smooth={true} duration={500}>
             Works
           </Link>
         </li>
-        <li className='py-6 text-4xl '>
-          <Link onClick={handleClick} to='contact' smooth={true} duration={500}>
+        <li className="py-6 text-4xl ">
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
             Contact
           </Link>
         </li>
       </ul>
 
       {/* social icons */}
-      <div className='hidden lg:flex fixed flex-col top-[35%] left-0 text-white'>
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0 text-white">
         <ul>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-700'>
-            <a href='https://www.linkedin.com/in/marufsiddique01/' target='_blank' className='flex justify-between items-center w-full text-grey-300' rel='noreferrer'>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-700">
+            <a
+              href="https://www.linkedin.com/in/marufsiddique01/"
+              target="_blank"
+              className="flex justify-between items-center w-full text-grey-300"
+              rel="noreferrer"
+            >
               Linkedin <FaLinkedin size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#c05f5f]'>
-            <a href='mailto:marufsiddique71@gmail.com' target='_blank' className='flex justify-between items-center w-full text-grey-300' rel='noreferrer'>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#c05f5f]">
+            <a
+              href="mailto:marufsiddique71@gmail.com"
+              target="_blank"
+              className="flex justify-between items-center w-full text-grey-300"
+              rel="noreferrer"
+            >
               Email <HiOutlineMail size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
-            <a href='https://github.com/marufsiddique01' target='_blank' className='flex justify-between items-center w-full text-grey-300' rel='noreferrer'>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
+            <a
+              href="https://github.com/marufsiddique01"
+              target="_blank"
+              className="flex justify-between items-center w-full text-grey-300"
+              rel="noreferrer"
+            >
               Github <FaGithub size={30} />
             </a>
           </li>
@@ -115,15 +140,20 @@ const Navbar = () => {
               Facebook <FaFacebook size={30} />
             </a>
           </li> */}
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#26b84a]'>
-            <a href='../assets/MarufSiddiqueResume.pdf' target='_blank' rel='noopener noreferrer' className='flex justify-between items-center w-full text-grey-300'>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#26b84a]">
+            <a
+              href="../assets/MarufSiddiqueResume.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex justify-between items-center w-full text-grey-300"
+            >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
           </li>
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
